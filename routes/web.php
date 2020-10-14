169b1c5ac90fbec8099934561a13bc60d84f0342
DESKTOP-EDSCJEB\ajibw <aji.bwi99@gmail.com>
2020-10-14 16:21:57 +0700

    task index berhasil

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,5 @@ Route::group(['prefix'=>'admin','middleware'=>'auth'], function(){
     Route::get('/', 'Admin\DashboardController@index');
     // Route::get('/kategori', 'Admin\KategoriController@index');
     Route::resource('/kategori', 'Admin\KategoriController');
+    Route::resource('/tugas', 'Admin\TugasController');
 });
